50d81c96219104c0b1c0e04daa38fc90db3871ed
Cami <camille@creativeambition.net>
2018-09-19 13:38:37 -0700

    tabindex on individual restaurants

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,6 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
   image.alt = restaurant.name;
+  image.tabIndex = 0;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
