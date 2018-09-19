07fcd9a8c925c82875e3b5272a75971388ff6bff
Cami <camille@creativeambition.net>
2018-09-19 13:36:39 -0700

    added title attr to gallery items

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.setAttribute('role', 'button');
+  more.setAttribute('title', restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
